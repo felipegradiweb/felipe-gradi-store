dd60f54d4e6215a0b7241c0c14bcf40917b89106
felipe190291 <pipe_cesped@hotmail.com>
2023-01-13 16:13:22 -0500

    changeprice

--- assets/pedir.js
+++ assets/pedir.js
@@ -1,3 +1,29 @@
-function window_loc(){
-console.log(window.location.href)
+function window_loc(id){
+    console.log('erere',id)
+    
+    var vaariantes=document.getElementById(id)
+    if(vaariantes){
+        console.log(vaariantes.dataset)
+         let identificador= vaariantes.dataset.identificador;
+         console.log(identificador)
+        let price= vaariantes.dataset.price;
+        console.log(price)
+        let ide='changediverprice-' + identificador
+     var pricechange=document.getElementById(ide)
+    if(pricechange)
+
+         pricechange.innerHTML =vaariantes.dataset.price;
+         
+      
+       
+    }
+//     for (let i = 0; i < vaariantes.children.length; i++) {
+// if(vaariantes.children[i].id ==id)
+//         console.log('yes')
+//     }
+    // fetch('/products/product-handle?section_id=product-template')
+
+    // .then(response => response.text())
+    
+    // .then(data => console.log(data));
 }
