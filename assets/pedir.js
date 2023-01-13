476530408c3024a6411b2c797a46ca0d6ce7f1fa
felipe190291 <pipe_cesped@hotmail.com>
2023-01-13 17:58:00 -0500

    cambiocolores

--- assets/pedir.js
+++ assets/pedir.js
@@ -9,21 +9,80 @@ function window_loc(id){
         let price= vaariantes.dataset.price;
         console.log(price)
         let ide='changediverprice-' + identificador
+        let pic='changepicture-' + identificador
      var pricechange=document.getElementById(ide)
-    if(pricechange)
+     var picturechange = document.getElementById(pic)
+    if(pricechange 
+        
+        || picturechange)
 
-         pricechange.innerHTML =vaariantes.dataset.price;
+         {
+            pricechange.innerHTML =vaariantes.dataset.price;
+            picturechange.src=vaariantes.dataset.picture;
+ 
+        }
          
       
        
     }
-//     for (let i = 0; i < vaariantes.children.length; i++) {
-// if(vaariantes.children[i].id ==id)
-//         console.log('yes')
-//     }
-    // fetch('/products/product-handle?section_id=product-template')
+  
 
-    // .then(response => response.text())
+}
+
+var all= document.querySelectorAll('.background-list_productspipe')
+if(all) {
+console.log(all.children)
+var childrens=all
+for (let i=0 ; i<childrens.length;i++){
+  
+        console.log(childrens[i])
+
+         let maspeque=childrens[i]
+         maspeque.addEventListener("mouseover", (e)=>{
+            var id=e.target.id
+            var otroid= e.target.id.replace('changepicture','changepicturehidden')
+            console.log(otroid)
+            var ap=document.getElementById(otroid)
+            if(otroid || ap){
+
+                ap.classList.remove('desaparecerimagen')
+                ap.classList.add('aparecerimagen')
+            }
+            if(id &&id.includes('changepicture')){
+                
+                var desap=document.getElementById(id)
+             if(desap){
+
+                 desap.classList.add('desaparecerimagen')
+             }
+            }
+          
+           
+          })
     
-    // .then(data => console.log(data));
+  
+          maspeque.addEventListener("mouseout", (e)=>{
+            var id=e.target.id
+            var otroid= e.target.id.replace('changepicturehidden','changepicture')
+            console.log(otroid,id)
+            var ap=document.getElementById(otroid)
+            if(otroid || ap){
+                ap.classList.remove('desaparecerimagen')
+                ap.classList.add('aparecerimagen')
+            }
+            if(id &&id.includes('changepicture')){
+                
+                var desap=document.getElementById(id)
+             if(desap){
+                desap.classList.remove('aparecerimagen')
+                 desap.classList.add('desaparecerimagen')
+             }
+            }
+          
+           
+          })
+    
+    ;}
+
+  
 }
