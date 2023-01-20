5e3dcdca0db171275a83c51a68f3adcf43991979
felipegradiweb <andres.cespedes@gradiweb.com>
2023-01-20 16:56:21 -0500

    ordercontinue

--- src/js/addcart.js
+++ src/js/addcart.js
@@ -1,6 +1,5 @@
 const responseModal = document.querySelector(".modal-container");
 const sectionfull = document.getElementById('allCartProducts')
-
 const totalCart = document.querySelector('.modal_unit')
 const continuaComprando=document.querySelectorAll('.continuaComprando')
 const sectionempty = document.getElementById('basketProductsEmpty')
@@ -8,31 +7,22 @@ const storageProductDiv = document.getElementById('storageProduct')
 const buttondelete=document.querySelectorAll('.button_delete') 
 const inputdown=document.querySelectorAll('.input-up-btn') 
 const inputup=document.querySelectorAll('.input-up-btn') 
-
-
-
+const clasebuttonCart=document.querySelectorAll('.addto-card')
 
 function iniciarcart(){
-
-const clasebuttonCart=document.querySelectorAll('.addto-card')
 clasebuttonCart.forEach((e,index)=>{
     e.addEventListener('click',(ele)=>{ 
       ele.preventDefault()
-      console.log('verlos',e,ele,index)
         agregarProductoAlCarrito(e.value)
     })
   })
   continuaComprando.forEach((e)=>{
     e.addEventListener('click',()=>{ 
         responseModal.classList.add('none')
-      
         sectionempty.classList.add('none')
-      
     })
-   
  })
-  function changeallproducts(collectionref,item){
-    console.log(collectionref[0])
+      function changeallproducts(collectionref,item){
     let collectionfactory =collectionref[0].children[0].children[2].children[0].children[0]
     collectionfactory.children[3].children[0].innerHTML='$' + item.final_line_price
     collectionfactory.children[2].children[1].value=item.quantity
@@ -43,9 +33,8 @@ clasebuttonCart.forEach((e,index)=>{
     collectionfactory.children[4].children[0].value=item.variant_id;
     collectionfactory.children[2].children[0].addEventListener('click',(e)=>{ 
       let inputselected=collectionfactory.children[2].children[0].parentNode.querySelector('input[type=number]')
-             let cantidad=inputselected.value
+      let cantidad=inputselected.value
       let identifi=inputselected.id.replace('input','')
- 
           changeProductCarrito(identifi,cantidad)
   
   
@@ -62,7 +51,7 @@ let identifi=inputselected.id.replace('input','')
       changeProductCarrito(e.target.value,0)
     })
   }
-async function agregarProductoAlCarrito(variant_id){ 
+      async function agregarProductoAlCarrito(variant_id){ 
       let data1 = {
         'items':[{
           'id': variant_id,
@@ -77,8 +66,7 @@ async function agregarProductoAlCarrito(variant_id){
         body: JSON.stringify(data1)
       })  .then((res) => res.json())
       .then(response => {
-        console.log(response.items[0].quantity)
-
+      
         let data = {
           'updates':{
              [`${response.items[0].id}`] :response.items[0].quantity
@@ -95,44 +83,31 @@ async function agregarProductoAlCarrito(variant_id){
           while (storageProductDiv.hasChildNodes()) {
             storageProductDiv.removeChild(storageProductDiv.firstChild);
           }
-      
           let items=   response.items
           for (let i=0 ; i<items.length;i++){
          
             var clone = sectionfull.cloneNode(true);
-         clone.classList.remove('none')
+            clone.classList.remove('none')
       
-         clone.id= 'misproductos-' + i;
+            clone.id= 'misproductos-' + i;
             var children = clone.childNodes;
             for (let index=children.length - 1; index>=0;index--){
-            // for (var index = 0; index < children.length; index++) {
               let collectionref=children[index].children
               if(collectionref){
                 changeallproducts(collectionref,items[i])
-          
               }
             }
-            
             storageProductDiv.appendChild(clone)
-      
-            console.log(items[i])  
           }
-           
           responseModal.classList.remove('none')
         })
         .catch((error) => {
-          
           console.log('Error:', error);
         });
       })
       .catch((error) => {
         console.log('Error:', error);
       });
-   
-    
-   
-    
-        
       }
       async function changeProductCarrito(variant_id,cantidad){ 
         let data = {
@@ -152,7 +127,7 @@ async function agregarProductoAlCarrito(variant_id){
             storageProductDiv.removeChild(storageProductDiv.firstChild);
           }
           let item_count =response.item_count;
-          console.log(item_count)
+     
           if(item_count &&item_count > 0){let items=   response.items
             for (let i=0 ; i<items.length;i++){
            
@@ -170,16 +145,10 @@ async function agregarProductoAlCarrito(variant_id){
               changeallproducts(collectionref,items[i])
                 }
               }
-              
               storageProductDiv.appendChild(clone)
-        
-              console.log(items[i])  
             }
-             
             responseModal.classList.remove('none') }else{
-       
             if(sectionfull || sectionempty){
-             
               sectionfull.classList.add('none')
               sectionempty.classList.remove('none')
             }
@@ -194,4 +163,6 @@ async function agregarProductoAlCarrito(variant_id){
         
       }
     }
-    iniciarcart()
+
+
+   iniciarcart()
--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,4 @@
-import "../styles.css";
+
 import './addcart'
 var storedProducts = JSON.parse(localStorage.getItem("storedProducts"));
 const sectionempty = document.getElementById('avisonoproducts')
@@ -6,7 +6,24 @@ const totalorders = document.getElementById('totalproducts')
 const totalcustomers = document.getElementById('totalcustomers')
 const deseos = document.getElementById('deseos')
 const heart_favorito=document.querySelectorAll('.button-favorite')
-function pruebaConsole(){
+const butoon_favorite= document.getElementById('gotoFavorite')
+const button_products= document.getElementById('goto-List-products')
+button_products &&button_products.addEventListener('click',()=>{
+    window.location.href='/'
+})
+butoon_favorite &&butoon_favorite.addEventListener('click',()=>{
+    window.location.href='/pages/FavoritosWish'
+})
+function probando(){
+  var aver=document.querySelectorAll('.averq')
+  aver.forEach(e=>{
+    e.addEventListener('click',(ele)=>{ 
+      ele.preventDefault()
+    })
+  })
+}
+
+function listadeseos(){
   deseos &&deseos.classList.remove('none')
     fetch('https://testeo-theta.vercel.app/allorders', {
         method: 'GET',
@@ -41,37 +58,40 @@ if (storedProducts == null || storedProducts.length === 0) {
     sectionempty &&sectionempty.classList.remove('none')
    
     storedProducts = [];
-    localStorage.setItem('storedProducts',JSON.stringify([]))
+ 
 }
+
+
+
 heart_favorito.forEach(e=>{
-    e.addEventListener('click',()=>{ 
+ var array =storedProducts.map(e=>e.id)
+
+  if(array.includes(e.value)){
+    e.classList.remove('opacity-low-heart')
+  }
+    e.addEventListener('click',(ele)=>{ 
         var product = {
             id: e.value
           };
-          let newstore=JSON.parse(localStorage.getItem("storedProducts"))
-          console.log(e)
-          console.log('revisemeeste',e.classList.contains('opacity-low-heart'))
+          var newstoreproductos=JSON.parse(localStorage.getItem("storedProducts")) || []
           if(e.classList.contains('opacity-low-heart')){
             e.classList.remove('opacity-low-heart')
-            newstore.push(product)
-             localStorage.setItem('storedProducts',JSON.stringify(newstore))
-          }else{
-            console.log('a ver pues')
+          
+            if(!array.includes(e.value)){
+              newstoreproductos.push(product)
+              localStorage.setItem('storedProducts',JSON.stringify(newstoreproductos))
+        }
+          }else{ e.classList.add('opacity-low-heart')
+            let removeArray=newstoreproductos.filter(element=>element.id !=e.value)
+            localStorage.setItem('storedProducts',JSON.stringify(removeArray))
           }
+         
     })
+   
+
   })  
 
 }
-pruebaConsole()
-const butoon_favorite= document.getElementById('gotoFavorite')
-const button_products= document.getElementById('goto-List-products')
-
-button_products &&button_products.addEventListener('click',()=>{
-    window.location.href='/'
-})
-butoon_favorite &&butoon_favorite.addEventListener('click',()=>{
-    window.location.href='/pages/FavoritosWish'
-})
 
 function verfavoritos(){
     const producto_favorito=document.querySelectorAll('.favorite-product')
@@ -91,15 +111,7 @@ function verfavoritos(){
             if(removeArray.length == 0){ sectionempty &&sectionempty.classList.remove('none')}
         })
      }) 
-    // heart_favorito.forEach((e)=>{
-    //     e.addEventListener('click',(ele)=>{ 
-    //       ele.preventDefault()
-    //     })
-    //     if(elementosarray.includes(e.value)){
-    //       console.log(e)
-    //         e.classList.remove('opacity-low-heart')
-    //            }
-    //  })  
+ 
         producto_favorito.forEach((e)=>{
          if(!elementosarray.includes(e.dataset.id)){
            
@@ -107,5 +119,6 @@ function verfavoritos(){
                 }
       })  
     }
-
-verfavoritos()
+    probando()
+    listadeseos()
+    verfavoritos()
